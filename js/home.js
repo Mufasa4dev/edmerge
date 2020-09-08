7625ee08c7cd1ee93846de97afe0d67f0ef05a74
hebraheem <iadebiyi01@gmail.com>
2020-09-08 23:40:17 +0100

    second statge of home page no bootstrap

--- js/home.js
+++ js/home.js
@@ -1,1 +1,58 @@
-let service
+let menuList = document.querySelector('.menuLink');
+let menuBtn = document.querySelector('.hide');
+menuBtn.addEventListener('click', displayMenu);
+
+function displayMenu() {
+    menuList.classList.toggle('showMenu')
+}
+
+// let droppDown = document.querySelector('.dropDown');
+// let dropdownShow = document.querySelector('.service');
+// dropdownShow.addEventListener('click', dropDownMenu);
+
+// function dropDownMenu(e){
+//     event.preventDefault()
+//     droppDown.style.display = "block"
+//     droppDown.classList.toggle("drop")
+// }
+
+
+
+// FOR SECTION LIVE OPPURTUNITIES
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:3,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[2000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
+
+//  FOR SECTION PARTNERS
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+
+
