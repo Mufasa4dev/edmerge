422ddacdd274a2f5f84de4340216767c1867ebd6
Ayodele Alademehin <ayodele.alademehin@gmail.com>
2020-09-12 22:33:23 -0700

    I just added Javascript function to the form

--- js/student.js
+++ js/student.js
@@ -1,1 +1,16 @@
-let mybtn = document.
+let mySignup = document.querySelector(".contents")
+let myShowreg = document.querySelector(".signin")
+let myLogin = document.querySelector(".details")
+let myShowlogin = document.querySelector(".signup")
+
+myShowreg.addEventListener("click", reg)
+function reg() {
+    mySignup.style.display ="none";
+    myLogin.style.display ="block";
+    
+}
+myShowlogin.addEventListener ("click", poplog)
+function poplog() {
+    myLogin.style.display ="none";
+    mySignup.style.display="block";
+}
