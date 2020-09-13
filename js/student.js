fe00b52105b36c27605ef5a1e79d6727d543f9e6
hebraheem <iadebiyi01@gmail.com>
2020-09-13 21:34:14 +0100

    fix ay's js

--- js/student.js
+++ js/student.js
@@ -1,16 +1,18 @@
 let mySignup = document.querySelector(".contents")
 let myShowreg = document.querySelector(".signin")
-let myLogin = document.querySelector(".details")
+let myLogin = document.querySelector("#maincontainer2")
 let myShowlogin = document.querySelector(".signup")
 
 myShowreg.addEventListener("click", reg)
-function reg() {
+function reg(e) {
+    event.preventDefault()
     mySignup.style.display ="none";
     myLogin.style.display ="block";
     
 }
 myShowlogin.addEventListener ("click", poplog)
-function poplog() {
+function poplog(e) {
+    event.preventDefault()
     myLogin.style.display ="none";
     mySignup.style.display="block";
 }
