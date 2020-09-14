fd4af89e6c2143ab5fecca31c5d13f235b123463
hebraheem <iadebiyi01@gmail.com>
2020-09-14 22:41:37 +0100

    workde on faq page

---
+++ js/myfaq.js
@@ -0,0 +1,123 @@
+      // let faqans= document.querySelectorAll('.faqans');
+        // let faqbtn = document.querySelectorAll('.btnfaq');
+        // for (j=0; j<faqbtn.length; j++){
+        // faqbtn[j].addEventListener('click', showans);}
+
+        // function showans(){
+        //     for (i = 0; i < faqans.length; i++){
+        //     if (faqans[i].style.display == "none"){
+        //     faqans[i].style.display = "block"
+        //     } else {
+        //         faqans[i].style.display = "none";
+        //     }
+        // }
+        // }
+        let faqans1= document.querySelector('#ans1');
+        let faqbtn1 = document.querySelector('#farqbtn1');
+        faqbtn1.addEventListener('click', showans1);
+
+        function showans1(){
+            if (faqans1.style.display == "none"){
+            faqans1.style.display = "block"
+            } else {
+                faqans1.style.display = "none";
+            }
+        
+        }
+        let faqans2= document.querySelector('#ans2');
+        let faqbtn2 = document.querySelector('#farqbtn2');
+        faqbtn2.addEventListener('click', showans2);
+
+        function showans2(){
+            if (faqans2.style.display == "none"){
+            faqans2.style.display = "block"
+            } else {
+                faqans2.style.display = "none";
+            }
+        
+        }
+        let faqans3= document.querySelector('#ans3');
+        let faqbtn3 = document.querySelector('#farqbtn3');
+        faqbtn3.addEventListener('click', showans3);
+
+        function showans3(){
+            if (faqans3.style.display == "none"){
+            faqans3.style.display = "block"
+            } else {
+                faqans3.style.display = "none";
+            }
+        
+        }
+        let faqans4= document.querySelector('#ans4');
+        let faqbtn4 = document.querySelector('#farqbtn4');
+        faqbtn4.addEventListener('click', showans4);
+
+        function showans4(){
+            if (faqans4.style.display == "none"){
+            faqans4.style.display = "block"
+            } else {
+                faqans4.style.display = "none";
+            }
+        
+        }
+        let faqans5= document.querySelector('#ans5');
+        let faqbtn5 = document.querySelector('#farqbtn5');
+        faqbtn5.addEventListener('click', showans5);
+
+        function showans5(){
+            if (faqans5.style.display == "none"){
+            faqans5.style.display = "block"
+            } else {
+                faqans5.style.display = "none";
+            }
+        
+        }
+        let faqans6= document.querySelector('#ans6');
+        let faqbtn6 = document.querySelector('#farqbtn6');
+        faqbtn6.addEventListener('click', showans6);
+
+        function showans6(){
+            if (faqans6.style.display == "none"){
+            faqans6.style.display = "block"
+            } else {
+                faqans6.style.display = "none";
+            }
+        
+        }
+        let faqans7= document.querySelector('#ans7');
+        let faqbtn7 = document.querySelector('#farqbtn7');
+        faqbtn7.addEventListener('click', showans7);
+
+        function showans7(){
+            if (faqans7.style.display == "none"){
+            faqans7.style.display = "block"
+            } else {
+                faqans7.style.display = "none";
+            }
+        
+        }
+
+        let faqans8= document.querySelector('#ans8');
+        let faqbtn8 = document.querySelector('#farqbtn8');
+        faqbtn8.addEventListener('click', showans8);
+
+        function showans8(){
+            if (faqans8.style.display == "none"){
+            faqans8.style.display = "block"
+            } else {
+                faqans8.style.display = "none";
+            }
+        
+        }
+        let faqans9= document.querySelector('#ans9');
+        let faqbtn9 = document.querySelector('#farqbtn9');
+        faqbtn9.addEventListener('click', showans);
+
+        function showans(){
+            if (faqans9.style.display == "none"){
+            faqans9.style.display = "block"
+            } else {
+                faqans9.style.display = "none";
+            }
+        
+        }
