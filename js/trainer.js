3917eaa68bf839e32be8b8090309cd6a999d2cef
Your Name <sotejubisola@gmail.com>
2020-09-16 08:33:58 +0100

    i reeditd the js page

--- js/trainer.js
+++ js/trainer.js
@@ -1,5 +1,5 @@
 // let myDetails = document.querySelector(".signup-details")
-let myDetails = document.querySelector(".signupd")
+let myDetails = document.querySelector(".showsignup")
 let showReg = document.querySelector(".signup");
 
 let myLogin = document.querySelector(".login");
