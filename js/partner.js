239f5e8130532edd313db944f57187d2358ad56b
hebraheem <iadebiyi01@gmail.com>
2020-09-17 18:40:56 +0100

    completed student sinup js

--- js/partner.js
+++ js/partner.js
@@ -15,3 +15,29 @@ function log(){
     myForm.style.display = "block";
     myLogin.style.display = "none";
 }
+
+//for password display
+let input = document.querySelector('.passbtn');
+let eye = document.querySelector('.eyeBtn');
+eye.addEventListener('click', showpass);
+
+function showpass() {
+    if (input.type == "password") {
+        input.type = "text"
+    } else {
+        input.type = "password"
+    }
+}
+
+
+let input1 = document.querySelector('.passbtn1');
+let eye1 = document.querySelector('.eyeBtn1');
+eye1.addEventListener('click', showpass1);
+
+function showpass1() {
+    if (input1.type == "password") {
+        input1.type = "text"
+    } else {
+        input1.type = "password"
+    }
+}
