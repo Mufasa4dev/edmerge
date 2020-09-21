4ea124290eddfad5f94f06bec27e367248afc64f
hebraheem <iadebiyi01@gmail.com>
2020-09-21 09:15:54 +0100

    worked on trainer team page

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-var app = new Framework7({
-  root: '#app',
-  name: 'My App',
-  id: 'com.myapp.test',
-  routes: [
-    // Add your routes here
-    // Example:
-    /*
-    {
-      path: '/about/',
-      url: 'about.html',
-    },
-    */
-  ],
-});
+// var app = new Framework7({
+//   root: '#app',
+//   name: 'My App',
+//   id: 'com.myapp.test',
+//   routes: [
+//     // Add your routes here
+//     // Example:
+//     /*
+//     {
+//       path: '/about/',
+//       url: 'about.html',
+//     },
+//     */
+//   ],
+// });
 
 var mainView = app.views.create('.view-main');
 
